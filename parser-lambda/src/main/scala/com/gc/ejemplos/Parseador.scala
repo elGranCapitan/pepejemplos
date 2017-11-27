5f418e02e70b14e6db71e417157cd50cbed4ae8e
Pepe <jca.formacion@gmail.com>
2017-11-27 01:31:31 +0100

    feat - parser recursivo con usando funcion lambda

---
+++ parser-lambda/src/main/scala/com/gc/ejemplos/Parseador.scala
@@ -0,0 +1,31 @@
+package com.gc.ejemplos
+
+import scala.annotation.tailrec
+
+object Parseador {
+
+  type TipoDeCorte = (String,String) => String
+
+  val haciaDelante: TipoDeCorte = (traza, etiqueta) => traza.substring(traza.indexOf(etiqueta) + etiqueta.length)
+  val paraAtras: TipoDeCorte = (traza, marcaFinal) => traza.split(marcaFinal).head.trim
+
+  @tailrec
+  def parserDeTraza(traza: String, etiquetas: Seq[String], marcaFinal: String): Option[String] = {
+    if (etiquetas.nonEmpty) {
+      cortarTraza(traza, etiquetas.head)(haciaDelante) match {
+        case Some(subTraza) => parserDeTraza(subTraza, etiquetas.tail, marcaFinal)
+        case None => parserDeTraza(traza, etiquetas.tail, marcaFinal)
+      }
+    } else {
+      cortarTraza(traza, marcaFinal)(paraAtras)
+    }
+  }
+
+  private def cortarTraza(traza: String, etiqueta: String)(tipoDeCorte: TipoDeCorte): Option[String] = {
+    traza.indexOf(etiqueta) match {
+      case position if position >= 0 => Some(tipoDeCorte(traza,etiqueta))
+      case -1 => None
+    }
+  }
+
+}
