f4129a14886e1fa008bc7f0086ad52883ffe7747
Pepe <jose.corredera@bluetab.net>
2017-11-28 10:17:30 +0100

    - sobrecarga del parser para reducir número de parametros

--- parser-lambda/src/test/scala/com/gc/ejemplos/ParseadorTest.scala
+++ parser-lambda/src/test/scala/com/gc/ejemplos/ParseadorTest.scala
@@ -16,12 +16,32 @@ class ParseadorTest extends FunSuite {
     assert(result==Some("una marca final"))
   }
 
+  test("logParser con todas las tags en la traza - parser 2 arg") {
+    val logTrace =
+      """esto es un log TAG1 que contiene varias etiquetas TAG2 y
+        |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG1","TAG2","TAG3","##"))
+
+    assert(result==Some("una marca final"))
+  }
+
   test("logParser con tags que no existen al principio de la listaen la traza") {
     val logTrace =
       """esto es un log TAG1 que contiene varias etiquetas TAG2 y
         |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
 
     val result = Parseador.parserDeTraza(logTrace,Seq("TAG5", "TAG1","TAG2","TAG3"),"##")
+
+    assert(result==Some("una marca final"))
+  }
+
+  test("logParser con tags que no existen al principio de la listaen la traza -parsere 2 arg") {
+    val logTrace =
+      """esto es un log TAG1 que contiene varias etiquetas TAG2 y
+        |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG5", "TAG1","TAG2","TAG3","##"))
 
     assert(result==Some("una marca final"))
   }
@@ -36,12 +56,32 @@ class ParseadorTest extends FunSuite {
     assert(result==Some("una marca final"))
   }
 
+  test("logParser con tags que no existen al final de la lista en la traza - parser 2 arg") {
+    val logTrace =
+      """esto es un log TAG1 que contiene varias etiquetas TAG2 y
+        |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG1","TAG2","TAG3","TAG5","##"))
+
+    assert(result==Some("una marca final"))
+  }
+
   test("logParser con tags que no existen al principio y al final de la lista en la traza") {
     val logTrace =
       """esto es un log TAG1 que contiene varias etiquetas TAG2 y
         |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
 
     val result = Parseador.parserDeTraza(logTrace,Seq("TAG9","TAG1","TAG2","TAG3","TAG5"),"##")
+
+    assert(result==Some("una marca final"))
+  }
+
+  test("logParser con tags que no existen al principio y al final de la lista en la traza - parser 2 arg") {
+    val logTrace =
+      """esto es un log TAG1 que contiene varias etiquetas TAG2 y
+        |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG9","TAG1","TAG2","TAG3","TAG5","##"))
 
     assert(result==Some("una marca final"))
   }
@@ -56,11 +96,31 @@ class ParseadorTest extends FunSuite {
     assert(result==Some("una marca final"))
   }
 
+  test("logParser con tags que no existen en cual quier posicion de la lista en la traza - parser 2 arg") {
+    val logTrace =
+      """esto es un log TAG1 que contiene varias etiquetas TAG2 y
+        |una TAG3 una marca final ## de hasta donde quiero parsear""".stripMargin
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG9","TAG1","TAG2","ALF4","TAG3","TAG5","##"))
+
+    assert(result==Some("una marca final"))
+  }
+
+
   test("logParser sin tags en la traza") {
     val logTrace =
       """esto es un log que contiene varias etiquetas y una marca final ## de hasta donde quiero parsear"""
 
     val result = Parseador.parserDeTraza(logTrace,Seq("TAG9","TAG1","TAG2","ALF4","TAG3","TAG5"),"##")
+
+    assert(result==Some("esto es un log que contiene varias etiquetas y una marca final"))
+  }
+
+  test("logParser sin tags en la traza - parser 2 arg") {
+    val logTrace =
+      """esto es un log que contiene varias etiquetas y una marca final ## de hasta donde quiero parsear"""
+
+    val result = Parseador.parserDeTraza(logTrace,Seq("TAG9","TAG1","TAG2","ALF4","TAG3","TAG5","##"))
 
     assert(result==Some("esto es un log que contiene varias etiquetas y una marca final"))
   }
